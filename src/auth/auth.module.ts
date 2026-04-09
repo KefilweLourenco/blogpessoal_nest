88375aa3f8bbe25dd102d22184166ba0628e29e2
Kefilwe Lourenco <kefilwebr@hotmail.com>
2026-04-09 18:25:42 -0300

    feat: auth

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { Module } from "@nestjs/common";
+
+import { Bcrypt } from "./bcrypt/bcrypt";
+ 
+@Module({
+
+    imports: [],
+
+    providers: [Bcrypt],
+
+    controllers: [],
+
+    exports: [Bcrypt],
+
+})
+
+export class AuthModule {};
+ 
